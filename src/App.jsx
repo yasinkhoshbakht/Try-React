24f33ea61d997d00f31566e7e3b66fb2d4492bb5
yasin_wolf <yasinkhoshabkht1400@gmail.com>
2024-08-20 23:35:55 +0330

    finished

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Gallry from "./Components/Gallery/Gallery";
+
+function App() {
+  return (
+    <div className="gallery">
+      <header>
+        <h1>My best wallpapers</h1>
+      </header>
+      <Gallry />
+      <br />
+      <br />
+      <h2>Hope to enjoy</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,34 @@
+import "./gallery.css";
+
+function Gallry() {
+  return (
+    <div className="images">
+      <img
+        src="http://ts1.explicit.bing.net/th?id=OIP.xCUo655CimhD90Nmy7llYQHaEK&pid=15.1"
+        alt=""
+      />
+      <img
+        src="http://www.bing.com/th?id=OIP.6eqWLgIILn4sfzgVpxqDZAHaEK&pid=15.1"
+        alt=""
+      />
+      <img
+        src="http://ts2.mm.bing.net/th?id=OIP.zvTH42gSclJtNydj25IWbAHaE7&pid=15.1"
+        alt=""
+      />
+      <img
+        src="http://www.bing.com/th?id=OIP.I4X_ilJ5O8dMg1yrVXovmQHaEo&pid=15.1"
+        alt=""
+      />
+      <img
+        src="http://ts4.mm.bing.net/th?id=OIP.PWuXwB81UP8Lj6q6-7ERDgHaEK&pid=15.1"
+        alt=""
+      />
+      <img
+        src="http://ts1.mm.bing.net/th?id=OIP.Ek61Ur6YfppYYf7hnp6BGgHaEK&pid=15.1"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Gallry;
